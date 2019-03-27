b6f6fd42621045730393654863df9e92828a0f87
Terrewee <terrewee@live.com>
2019-03-27 16:28:31 +0100

    Update driving.cpp

--- driving.cpp
+++ driving.cpp
@@ -10,36 +10,107 @@ BrickPi3 BP;
 
 void exit_signal_handler(int signo);
 
+int masterRotate = 150;
+int masterForward = 200;
+
 //Stop
 void stop(void){
         BP.set_motor_power(PORT_B, 0);
         BP.set_motor_power(PORT_C, 0);
 }
 //forward
-void fwd(void){
-        BP.set_motor_dps(PORT_B, 360);
-        BP.set_motor_dps(PORT_C, 360);
-        usleep(500000);
+void fwd(int &powerA, int &powerB){
+    if      (powerA > masterForward){powerA -= masterRotate;}
+    else if (powerA < masterForward){powerA += masterRotate;}
+    if      (powerB > masterForward){powerB -= masterRotate;}
+    else if (powerB < masterForward){powerB += masterRotate;}
 
+    BP.set_motor_dps(PORT_B, powerA);
+    BP.set_motor_dps(PORT_C, powerB);
 }
 //move left
-void leftcorrectie(void){
-        BP.set_motor_dps(PORT_B, 180);
-        BP.set_motor_dps(PORT_C, -180);
+void leftcorrectie(int &powerA, int &powerB){
+    powerA += masterRotate;
+    powerB -= masterRotate;
+
+    BP.set_motor_dps(PORT_B, powerA);
+    BP.set_motor_dps(PORT_C, powerB);
+}
+
+void slightLeft(int &powerA, int &powerB){
+    powerA += (masterRotate/2);
+    powerB -= (masterRotate/2);
+
+    BP.set_motor_dps(PORT_B, powerA);
+    BP.set_motor_dps(PORT_C, powerB);
 }
 
 //Move Right
-void rightcorrectie(void){
-        BP.set_motor_dps(PORT_B, -180);
-        BP.set_motor_dps(PORT_C, 180);
+void rightcorrectie(int &powerA, int &powerB){
+    powerA -= masterRotate;
+    powerB += masterRotate;
+
+    BP.set_motor_dps(PORT_B, powerA);
+    BP.set_motor_dps(PORT_C, powerB);
 }
 
-void measure(sensor_color_t Color1, sensor_color_t Color4){
+void slightRight(int &powerA, int &powerB){
+    powerA -= (masterRotate/2);
+    powerB += (masterRotate/2);
+
+    BP.set_motor_dps(PORT_B, powerA);
+    BP.set_motor_dps(PORT_C, powerB);
+}
+
+bool checkObstacleInRange(sensor_ultrasonic_t ultrasonic, int &powerA, int &powerB){
+    
+    while(true){
+    int detectRange = 5;
+        cout << "cm " << ultrasonic.cm << endl;
+        if(detectRange == ultrasonic.cm){stop();}
+        else{fwd(powerA, powerB);}
+        if(BP.get_sensor(PORT_2, ultrasonic) == 0){
+            if(ultrasonic.cm <= detectRange+1 && ultrasonic.cm > 0){stop();}
+            else if(detectRange < ultrasonic.cm+1 && ultrasonic.cm+2 > 0){fwd(powerA, powerB); break;}
+        }
+        else{cout << "Error -7: Ultrasonic sensor not properly connected or initialized.";}
+        sleep(1);
+    }
+}
+
+void crossroad(int &powerA, int &powerB){
+    BP.set_motor_dps(PORT_B, 0);
+    BP.set_motor_dps(PORT_C, 0);
+    char keuze;
+    cout << "Links: A, Rechts: D, vooruit: W " << endl;
+    cin >> keuze;
+    if(keuze == 'A' || keuze == 'a'){
+        BP.set_motor_dps(PORT_B, 180);
+        BP.set_motor_dps(PORT_C, -180);
+    }
+    if(keuze == 'D' || keuze == 'd'){
+        BP.set_motor_dps(PORT_B, -180);
+        BP.set_motor_dps(PORT_C, 180);
+    }
+    if(keuze == 'W' || keuze == 'w'){
+        fwd(powerA, powerB);
+    }
+    usleep(500000);  
+}
+
+void measure(sensor_color_t Color1, sensor_color_t Color4, int powerA, int powerB, sensor_ultrasonic_t ultrasonic, int &ticker){
+    if(ticker == 100){checkObstacleInRange(ultrasonic, powerA, powerB); ticker = 0;}
+    
     if((BP.get_sensor(PORT_1, Color1) == 0)&&(BP.get_sensor(PORT_4, Color4) == 0)){
-        if((Color1.color == 1 )&&(Color4.color == 6)){rightcorrectie();}       //rechts wit links zwart
-        if((Color1.color == 6)&&(Color4.color == 1)){leftcorrectie();}         //rechts zwart links wit
-        if((Color1.color == 6)&&(Color4.color == 6)){fwd();}                   //rechts zwart 
+        if      (Color1.color == 1 && Color4.color == 6)                        {rightcorrectie(powerA, powerB);}       //rechts wit links zwart
+        else if ((Color4.color > 3 && Color4.color < 6) && Color1.color == 1)   {slightRight(powerA, powerB);}
+        else if (Color1.color == 6 && Color4.color == 1)                        {leftcorrectie(powerA, powerB);}         //rechts zwart links wit
+        else if ((Color1.color > 3 && Color1.color < 6) && Color4.color == 1)   {slightLeft(powerA, powerB);}
+        else if (Color1.color == 6 && Color4.color == 6 )                       {fwd(powerA, powerB);}                   //rechts zwart 
+        else if (Color1.color == 1 && Color4.color == 1)                        {crossroad(powerA, powerB);}
         cout << "Color1 " << (int) Color1.color << " Color4 " << (int) Color4.color << endl;
+        ticker++;
+        cout << "ticker: " << ticker << endl;
     }
 }
 
@@ -50,17 +121,22 @@ int main(){
 
     BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_FULL);
     BP.set_sensor_type(PORT_4, SENSOR_TYPE_NXT_COLOR_FULL);
+    BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_ULTRASONIC);
 
     sensor_color_t      Color1;
     sensor_color_t      Color4;
+    sensor_ultrasonic_t ultrasonic;
 
-    BP.set_motor_power(PORT_B, 20);
-    BP.set_motor_power(PORT_C, 22);
+    BP.set_motor_power(PORT_B, 40);
+    BP.set_motor_power(PORT_C, 44);
 
-    measure(Color1,Color4);
-    while(true){
-        measure(Color1, Color4);
-    }
+    int powerA = masterForward;
+    int powerB = masterForward;
+
+    int ticker = 0;
+
+    measure(Color1,Color4, powerA, powerB, ultrasonic, ticker);
+    while(true){measure(Color1, Color4, powerA, powerB, ultrasonic, ticker);}
 }
 
 void exit_signal_handler(int signo){
